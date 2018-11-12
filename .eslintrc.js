b58b7e7a6b1bb53a2b2a9a8a661cdb46206913f7
王闯 <865748877@qq.com>
2018-11-12 20:44:17 +0800

    .

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+      "es6": true,
+      "commonJs":true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+      "ecmaVersion": 2016,
+      "sourceType": "script"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+      ],
+      "no-console": [
+        "error",
+        { allow: ["warn", "error","info"] }]
+    }
+};
