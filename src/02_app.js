866e62de42fdafc631cdd0826a309fc0ab6320c6
王闯 <865748877@qq.com>
2018-12-25 00:20:14 +0800

    chore:study

--- src/02_app.js
+++ src/02_app.js
@@ -1,12 +1,17 @@
 const http = require('http');
 const conf = require('./config/defaultConfig');
-const path = require('path');
 const chalk = require('chalk');
-const route = require('./helper/route');
 
 const server = http.createServer((req, res) => {
-  const filePath = path.join(conf.root, req.url);
-  route(req, res, filePath);
+  res.statusCode = 200;//响应状态码
+  res.setHeader('Content-Type', 'text/html');//设置响应头
+  res.write('<html>');
+  res.write('<body>');
+  res.write('<h1 style="color:yellow">');
+  res.write('hello http~~');
+  res.write('</h1>');
+  res.write('</body>');
+  res.end('</html>');
 });
 
 server.listen(conf.port, conf.hostname, () => {
---
+++ src/helper/route.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const Handlebars = require('handlebars');
+const path = require('path');
+const promisify = require('util').promisify;
+const stat = promisify(fs.stat);
+const readdir = promisify(fs.readdir);
+const config = require('../config/defaultConfig');
+
+const tplPath = path.join(__dirname, '../template/dir.tpl');
+const source = fs.readFileSync(tplPath);
+const template = Handlebars.compile(source.toString());
+
+module.exports = async function (req, res,filePath) {
+  try {
+    const stats = await stat(filePath);
+    if (stats.isFile()) {
+      res.statusCode = 200;
+      res.setHeader('Content-type', 'text/plain');
+      fs.createReadStream(filePath).pipe(res);
+    } else if (stats.isDirectory()) {
+      const files = await readdir(filePath);
+      const dir = path.relative(config.root, filePath);
+      res.statusCode = 200;
+      res.setHeader('Content-type', 'text/html');
+      const data = {
+        title: path.basename(filePath),
+        dir:dir ? `/${dir}` : '',
+        files
+      };
+      res.end(template(data));
+    }
+  }
+  catch (ex) {
+    console.log(ex);
+    res.statusCode = 404;
+    res.setHeader('Content-type', 'text/plain');
+    res.end(`${filePath} is not a directory or file`);
+  }
+}
